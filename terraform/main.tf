3087985946a1b69a62b0c5eab70bf6058fa8dd41
Joe M <joe@Joes-MacBook-Pro.local>
2022-05-16 18:51:34 +0800

    creating first account through aft

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,20 +2,20 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    AccountEmail              = "jm.pcloud22+dev_env@gmail.com"
+    AccountName               = "Development"
+    ManagedOrganizationalUnit = "Application_Environment"
+    SSOUserEmail              = "jm.pcloud22+dev_env@gmail.com"
+    SSOUserFirstName          = "jm"
+    SSOUserLastName           = "pcloud22"
   }
 
   account_tags = {
-    "Learn Tutorial" = "AFT"
+    "Environment" = "Development"
   }
 
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
+    change_requested_by = "Joe M"
     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
   }
 
@@ -23,5 +23,5 @@ module "sandbox" {
     group = "non-prod"
   }
 
-  account_customizations_name = "sandbox"
+  account_customizations_name = "Application_Development"
 }
